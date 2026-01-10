c809dbbb24a0fbf6201d494b74eceb9610d557c9
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-10 13:54:46 +0000

    feat:Image in components

--- components/MDXContent.tsx
+++ components/MDXContent.tsx
@@ -1,9 +1,14 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Image from "next/image";
+
+const components = {
+  Image,
+};
 
 export default function MDXContent({ source }: { source: string }) {
   return (
     <article className="prose prose-neutral dark:prose-invert max-w-none">
-      <MDXRemote source={source} />
+      <MDXRemote source={source} components={components}/>
     </article>
   );
 }
