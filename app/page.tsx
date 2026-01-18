bd9672382f9ee4c433004655fafca87033a70768
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-18 17:46:50 +0000

    feat:compoents add

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import BlogIndex from './blog/page'
+import Home from '@/components/home/home'
+import Projects from '@/components/projects/Projects'
+import OnekoCat from '@/components/common/OnekoCat'
 
 const page = () => {
   return (
-     <main className="prose dark:prose-invert max-w-3xl mx-auto px-4 py-12">
-      <BlogIndex />
+    <main className="prose dark:prose-invert max-w-3xl mx-auto px-4 py-12 ">
+      <Home />
+      <Projects />
+      <OnekoCat />
     </main>
   )
 }
