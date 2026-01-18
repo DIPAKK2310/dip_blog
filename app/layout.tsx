f78287bb78248657f89fe41b3d1c429ec049a606
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-18 17:45:55 +0000

    feat:quote and analytics add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,64 +1,39 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/react';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+import { Quote } from '@/components/common/Quote'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://dipnext.vercel.app/'),
   alternates: {
-    canonical: '/'
+    canonical: '/',
   },
   title: {
     default: 'Dipak Khare',
-    template: '%s | Dipak Khare'
+    template: '%s | Dipak Khare',
   },
-  description: 'My portfolio, blog, and personal website.'
-};
+  description: 'My portfolio, blog, and personal website.',
+}
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${inter.className}`}>
-      <body className="antialiased tracking-tight">
-        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 dark:bg-zinc-950 bg-white text-gray-900 dark:text-zinc-200">
+    <html lang="en" className={inter.className}>
+      <body className="antialiased tracking-tight bg-black text-zinc-200 min-h-screen">
+        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-black text-zinc-200">
           <main className="max-w-[60ch] mx-auto w-full space-y-6">
             {children}
+            <Quote />
           </main>
-          <Footer />
           <Analytics />
         </div>
       </body>
     </html>
-  );
-}
-
-function Footer() {
-  const links = [
-    { name: '@dipakkhare_23', url: 'https://x.com/dipakkhare_23' },
-    { name: 'linkedin', url: 'https://www.linkedin.com/in/dipak-khare-159107212/' },
-    { name: 'github', url: 'https://github.com/DIPAKK2310' }
-  ];
-
-  return (
-    <footer className="mt-12 text-center">
-      <div className="flex justify-center space-x-4 tracking-tight">
-        {links.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 dark:text-gray-500 hover:text-blue-500 transition-colors duration-200"
-          >
-            {link.name}
-          </a>
-        ))}
-      </div>
-    </footer>
-  );
+  )
 }
