97ced1d91f203227543fd096f247c67310475c96
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-29 18:45:37 +0000

    feat:hero config

---
+++ config/Hero.ts
@@ -0,0 +1,41 @@
+export const heroConfig = {
+  // Personal Information
+  name: 'Dipak Khare',
+  title: 'A Full Stack web developer & Devops Enthusiast.',
+  avatar: '/assets/logo2.png',
+
+  // Skills Configuration
+  skills: [
+    {
+      name: 'Typescript',
+      href: 'https://www.typescriptlang.org/',
+      component: 'TypeScript',
+    },
+    {
+      name: 'React',
+      href: 'https://react.dev/',
+      component: 'ReactIcon',
+    },
+    {
+      name: 'Next.js',
+      href: 'https://nextjs.org/',
+      component: 'NextJs',
+    },
+    {
+      name: 'Bun',
+      href: 'https://bun.sh/',
+      component: 'Bun',
+    },
+    {
+      name: 'PostgreSQL',
+      href: 'https://www.postgresql.org/',
+      component: 'PostgreSQL',
+    },
+  ],
+
+  // Description Configuration
+  description: {
+    template:
+      'I build interactive web apps using {skills:0}, {skills:1}, {skills:2}, {skills:3} and {skills:4}. With a focus on <b>UI</b> design. Enthusiastic about <b>Three.js</b>, driven by a keen eye for design.',
+  },
+}
