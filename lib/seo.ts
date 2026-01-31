818e93ef6c36572384ec003b521c6e06e39c2860
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-31 14:46:04 +0000

    lib:seo update

--- lib/seo.ts
+++ lib/seo.ts
@@ -125,8 +125,14 @@ export function generateSEO(pathname: string): Metadata {
     robots: {
       index: true,
       follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+        'max-video-preview': -1,
+      },
     },
-
     alternates: {
       canonical: url,
     },
