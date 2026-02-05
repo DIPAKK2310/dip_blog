cf3a343eb6696b10ddacc839858ff713d22f4d8d
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-05 19:12:36 +0000

    feat:upstashdb add

--- app/api/visit/route.ts
+++ app/api/visit/route.ts
@@ -1,7 +1,12 @@
-import { kv } from '@vercel/kv'
+import { Redis } from '@upstash/redis'
 import { NextResponse } from 'next/server'
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
 export async function GET() {
-  const count = await kv.incr('site_visits')
+  const count = await redis.incr('site_visits')
   return NextResponse.json({ count })
 }
