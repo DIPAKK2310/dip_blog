e583cd47b04c5762000ca1e7378aba43211a459c
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-06 18:04:21 +0000

    feat:visitor counter add

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import BlogIndex from '@/app/blog/page'
+import VisitorCounter from '../common/VisitorCounter'
 
 const Home = () => {
   const hoverClass =
     'text-zinc-200 transition-colors duration-200 hover:bg-gradient-to-r hover:from-orange-400 hover:to-amber-400 hover:bg-clip-text hover:text-transparent hover:underline underline-offset-4 decoration-orange-400/40 decoration-2 cursor-pointer'
 
   return (
-    <div className="space-y-5 leading-relaxed text-zinc-200 text-xl">
-      <p>Hi There!</p>
+    <div className="space-y-5 max-w-3xl mx-auto leading-relaxed text-zinc-200 text-xl">
+      <div className="flex items-center justify-between">
+        <p className="text-2xl font-medium">Hi There!</p>
+        <VisitorCounter />
+      </div>
 
       <p>
         I&apos;m Dipak Khare, a{' '}
