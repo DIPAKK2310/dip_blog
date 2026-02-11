b4c0e061d5e6c4c8919a68040da2b1a10edb3b3d
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-11 17:35:25 +0000

    feat:sitemap add for site

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://dipnext.vercel.app/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://dipnext.vercel.app/blog',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yourdomain.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://dipnext.vercel.app/devops',
+      lastModified: new Date(),
+    },
+  ]
+}
