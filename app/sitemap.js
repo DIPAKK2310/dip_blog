1d2f4927a66b0222fe47e754ff27403ed85a44de
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-11 17:52:48 +0000

    feat:sitemap for every page

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,14 @@
-export default function sitemap() {
+export default async function sitemap() {
+  const posts = await getPosts()
+
+  const postUrls = posts.map((post) => ({
+    url: `https://dipnext.vercel.app/blog/${post.slug}`,
+    lastModified: new Date(post.updatedAt),
+  }))
+
   return [
     {
-      url: 'https://dipnext.vercel.app/',
+      url: 'https://dipnext.vercel.app',
       lastModified: new Date(),
     },
     {
@@ -9,12 +16,14 @@ export default function sitemap() {
       lastModified: new Date(),
     },
     {
-      url: 'https://yourdomain.com/about',
+      url: 'https://dipnext.vercel.app/about',
       lastModified: new Date(),
     },
     {
       url: 'https://dipnext.vercel.app/devops',
       lastModified: new Date(),
     },
+
+    ...postUrls,
   ]
 }
