7c6c4b059211ae67d4736f3720d72de60009e40f
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-27 17:57:16 +0000

    feat:HeroDev add

--- app/devops/page.tsx
+++ app/devops/page.tsx
@@ -1,23 +1,11 @@
+import HeroDev from '@/components/devops/HeroDev'
 import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="https://www.docker.com/">Docker</Link>
-        </li>
-        <li>
-          <Link href="https://kubernetes.io/">Kubernetes</Link>
-        </li>
-        <li>
-          <Link href="https://github.com/features/actions">Github-Actions</Link>
-        </li>
-        <li>
-          <Link href="/">More..</Link>
-        </li>
-      </ul>
+     <HeroDev/>
     </div>
   )
 }
