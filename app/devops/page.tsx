802a8e45dfddf0efff2bc99d1eeb04ccee48ae00
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-28 10:08:37 +0000

    feat:Arrange page

--- app/devops/page.tsx
+++ app/devops/page.tsx
@@ -1,4 +1,8 @@
+import Ci_Cd from '@/components/devops/Ci_Cd'
+import Deployments from '@/components/devops/Deployments'
 import HeroDev from '@/components/devops/HeroDev'
+import Infra from '@/components/devops/Infra'
+import Tools from '@/components/devops/TechBadges'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,6 +10,9 @@ const page = () => {
   return (
     <div>
      <HeroDev/>
+      <Deployments/>
+      <Ci_Cd/>
+      <Infra/>
     </div>
   )
 }
