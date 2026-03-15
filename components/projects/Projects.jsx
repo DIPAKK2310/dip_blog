6f14bb8949b25872824f2a051ec72141ca46cc68
DIPAKK2310 <dipakkhare217@gmail.com>
2026-03-15 07:02:54 +0000

    refractor:add description to projects

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 
 const Projects = () => {
   const projects = [
-    { name: 'Nike 2.0', url: 'https://nike-landing-vert-nine.vercel.app/' },
-    { name: 'Dipk.me', url: 'https://github.com/DIPAKK2310/dipk.me' },
-    { name: 'ShoppYnow', url: 'https://github.com/DIPAKK2310/ShoppYnow' },
-    { name: 'Lumina', url: 'https://github.com/DIPAKK2310/lumina' },
+    {
+      name: 'Nike 2.0',
+      description:
+        'Modern e-commerce landing page built with React, Tailwind CSS, and Framer Motion, featuring responsive design and smooth UI animations.',
+      url: 'https://nike-landing-vert-nine.vercel.app/',
+    },
+    {
+      name: 'Dipk.me',
+      description:
+        'Personal developer portfolio and blogging platform built with Next.js and MDX, featuring command palette navigation and performance-focused design.',
+      url: 'https://github.com/DIPAKK2310/dipk.me',
+    },
+    {
+      name: 'ShoppYnow',
+      description:
+        'Full-stack e-commerce application with product browsing, authentication, and admin management built using the MERN stack.',
+      url: 'https://github.com/DIPAKK2310/ShoppYnow',
+    },
+    {
+      name: 'Lumina',
+      description:
+        'Modern responsive web interface built with React and Tailwind CSS focusing on clean UI, reusable components, and smooth user experience.',
+      url: 'https://github.com/DIPAKK2310/lumina',
+    },
   ]
 
   const itemClass =
@@ -16,17 +36,21 @@ const Projects = () => {
       <h3 className="text-zinc-200 text-3xl font-bold mb-3">Projects</h3>
 
       <div className="max-w-3xl mx-auto py-2">
-        <ul className="text-xl space-y-4  list-none list-inside">
+        <ul className="text-xl space-y-6 list-none">
           {projects.map((project) => (
-            <li key={project.name} className={itemClass}>
+            <li key={project.name} className="group">
               <a
-                className={itemClass}
+                className="text-zinc-200 transition hover:text-yellow-400 border-b border-transparent "
                 href={project.url}
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 {project.name}
               </a>
+
+              <p className="text-zinc-200 text-sm mt-1">
+                {project.description}
+              </p>
             </li>
           ))}
         </ul>
