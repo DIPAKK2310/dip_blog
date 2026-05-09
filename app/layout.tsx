b42065e1f93dd8dd3e4846885bc11413106c3f9d
DIPAKK2310 <dipakkhare217@gmail.com>
2026-05-09 09:05:37 +0000

    feat:icon add

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,17 @@ export const metadata: Metadata = {
   },
   description:
     'Dipak Khare is a Full Stack Developer specializing in Next.js, MERN Stack, and DevOps tools like Docker, Kubernetes, and GitHub Actions. Read blogs and explore projects.',
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
   keywords: [
     'Dipak Khare',
     'Full Stack Developer',
